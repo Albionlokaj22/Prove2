abca679a0d61867314b9832bdcfa00dd4f96ca96
albionlokaj22 <albionlokaj00@gmail.com>
2024-12-09 18:22:03 +0100

    Initial Commit

---
+++ Prove2/Prove2App.swift
@@ -0,0 +1,17 @@
+//
+//  Prove2App.swift
+//  Prove2
+//
+//  Created by Albion Lokaj on 9.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Prove2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Prove2Tests/Prove2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Prove2Tests.swift
+//  Prove2Tests
+//
+//  Created by Albion Lokaj on 9.12.24.
+//
+
+import Testing
+@testable import Prove2
+
+struct Prove2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
